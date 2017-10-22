d710b46adb30be53b0c287e1cd0e9c389d6c51df
piojoch <32995646+piojoch@users.noreply.github.com>
2017-10-22 09:49:06 +0200

    Init

---
+++ TheWorld/wwwroot/js/site.js
@@ -0,0 +1,33 @@
+﻿// site.js
+(function () {
+    //var ele = $("#userName");
+    //ele.text("Piotr Jochymczyk");
+
+    //var main = $("#main");
+    //main.on("onmouseenter", function () {
+    //    main.style = "background-color: #888;";
+    //});
+    //main.on("onmouseleave", function () {
+    //    main.style = "";
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        }
+        else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+    
+})();
