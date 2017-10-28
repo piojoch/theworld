1ea0dd5fe06c6e1e8c6815a5d091d9f013505fd0
piojoch <32995646+piojoch@users.noreply.github.com>
2017-10-28 20:05:11 +0200

    Complete module

---
+++ TheWorld/wwwroot/js/app-trips.js
@@ -0,0 +1,7 @@
+﻿// app-trips.js
+(function () {
+
+    "use scrict";
+    angular.module("app-trips", ["simpleControls"]);
+
+})();
---
+++ TheWorld/wwwroot/js/tripsController.js
@@ -0,0 +1,50 @@
+﻿// tripsController.js
+(function () {
+
+    "use scrict";
+
+    angular.module("app-trips")
+        .controller("tripsController", tripsController);
+
+    function tripsController($http) {
+        var viewModel = this;
+
+        viewModel.trips = [];
+
+        viewModel.newTrip = {};
+
+        viewModel.errorMessage = "";
+        viewModel.isBussy = true;
+
+        $http.get("/api/trips")
+            .then
+            (
+            function (response) {
+                angular.copy(response.data, viewModel.trips);
+            },
+            function (error) {
+                viewModel.errorMessage = "Failed to load data: " + error;
+            }
+            )
+            .finally(function () {
+                viewModel.isBussy = false;
+            });
+
+        viewModel.addTrip = function () {
+            viewModel.isBussy = true;
+            viewModel.errorMessage = "";
+
+            $http.post("/api/trips", viewModel.newTrip)
+                .then(function (response) {
+                    viewModel.trips.push(response.data);
+                    viewModel.newTrip = {};
+                }, function (error) {
+                    viewModel.errorMessage = "Failed to save new trip: " + error;
+                })
+                .finally(function () {
+                    viewModel.isBussy = false;
+                });
+        };
+    }
+
+})();
